ca9a8193a12f6f76f5caf8c14f966892c8c5907d
Abhijitkashyap97 <abhijitkashyap97@gmail.com>
2024-10-27 10:20:29 +0530

    Fix deployment issues

--- index.js
+++ index.js
@@ -35,7 +35,10 @@ app.use(express.static('public', {
       }
   }
 }));
-
+app.get('/files/:filename', (req, res) => {
+  const filename = decodeURIComponent(req.params.filename);
+  res.sendFile(path.join(__dirname, 'public', 'files', filename));
+});
 // Configure for larger file uploads if needed
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
